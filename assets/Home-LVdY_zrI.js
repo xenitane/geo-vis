2f5b3a0db166ac370e279cf22d9ca447f41f9590
xenitane <xenitane@users.noreply.github.com>
2024-01-10 16:13:41 +0000

    deploy: 8cadbba9ce72385c950cdb6d4aa1b06a4e5ea564

---
+++ assets/Home-LVdY_zrI.js
@@ -0,0 +1,8 @@
+import{j as e,S as a,r as t,_ as l}from"./index-PKfJwdl6.js";import{L as o}from"./Linear-b5mEHpys.js";const c={linear:o},n=()=>e.jsxs("div",{className:" space-y-4",children:[e.jsx(a,{className:"h-9 w-64 rounded-2xl"}),e.jsx(a,{className:"h-0.5 w-full"})]}),i=t.lazy(()=>l(()=>import("./GeoSection-3oiIyWlA.js"),__vite__mapDeps([0,1,2]))),p=()=>e.jsx("article",{className:"flex flex-col gap-8 py-2",children:Object.entries(c).map(([s,r])=>e.jsx(t.Suspense,{fallback:e.jsx(n,{}),children:e.jsx(i,{sectionId:s,sectionType:s,objectList:r})},s))});export{p as default};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/GeoSection-3oiIyWlA.js","assets/index-PKfJwdl6.js","assets/index-oQOWz8jY.css"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+//# sourceMappingURL=Home-LVdY_zrI.js.map
