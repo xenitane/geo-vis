8605bb3692288ad7eaf9cedb15fc3d1e1ab5fc70
xenitane <xenitane@users.noreply.github.com>
2024-01-12 04:15:11 +0000

    deploy: 0b03d6e5603754e25b5902b1f8132ee31e69bf5a

--- assets/Home-Bj_yIm-g.js
+++
@@ -1,8 +0,0 @@
-import{j as e,S as a,r as t,_ as l}from"./index-pT_iBuUB.js";import{L as o}from"./Linear-B5dr8mxP.js";const c={linear:o},n=()=>e.jsxs("div",{className:" space-y-4",children:[e.jsx(a,{className:"h-9 w-64 rounded-2xl"}),e.jsx(a,{className:"h-0.5 w-full"})]}),i=t.lazy(()=>l(()=>import("./GeoSection-WtQ4LbT2.js"),__vite__mapDeps([0,1,2]))),p=()=>e.jsx("article",{className:"flex flex-col gap-8 py-2",children:Object.entries(c).map(([s,r])=>e.jsx(t.Suspense,{fallback:e.jsx(n,{}),children:e.jsx(i,{sectionId:s,sectionType:s,objectList:r})},s))});export{p as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/GeoSection-WtQ4LbT2.js","assets/index-pT_iBuUB.js","assets/index-wEAnLqxI.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-//# sourceMappingURL=Home-Bj_yIm-g.js.map
