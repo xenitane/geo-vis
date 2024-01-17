39a38d01572546a60aa75fb7f8b5904f7966f3af
Tushar Jain | xenitane <tushar01.tjdsk@gmail.com>
2024-01-17 16:26:18 +0530

    added new fill fractal

--- src/lib/rules/Fill.ts
+++ src/lib/rules/Fill.ts
@@ -1,6 +1,6 @@
 import { fibonacciWordFractal } from "@/assets/Thumbnails";
 import { FillFractalInfo, Point } from "@/types";
-import { add, rotate } from "../utils";
+import { add, multiply, rotate } from "../utils";
 
 function genCentersV(centers: Point[], sides: number, i_rad: number, shift = 0) {
 	const newCenters: Point[] = [];
@@ -14,12 +14,12 @@ function genCentersV(centers: Point[], sides: number, i_rad: number, shift = 0) 
 	return newCenters;
 }
 
-function genCentersVC(centers: Point[], invCenters: Point[], sides: number, i_rad: number): [Point[], Point[]] {
+function genCentersVC(centers: Point[], invCenters: Point[], sides: number, i_rad: number, shift = 0): [Point[], Point[]] {
 	const newCenters: Point[] = [];
 	const newInvCenters: Point[] = [];
 	for (const center of centers) {
 		newInvCenters.push(center);
-		let dir: Point = [0, -i_rad];
+		let dir: Point = rotate([0, -i_rad], shift);
 		for (let j = 0; j < sides; j++) {
 			newCenters.push(add(center, dir));
 			dir = rotate(dir, (2 * Math.PI) / sides);
@@ -27,7 +27,7 @@ function genCentersVC(centers: Point[], invCenters: Point[], sides: number, i_ra
 	}
 	for (const invCenter of invCenters) {
 		newCenters.push(invCenter);
-		let dir: Point = [0, i_rad];
+		let dir: Point = rotate([0, i_rad], shift);
 		for (let j = 0; j < sides; j++) {
 			newInvCenters.push(add(invCenter, dir));
 			dir = rotate(dir, (2 * Math.PI) / sides);
@@ -35,6 +35,21 @@ function genCentersVC(centers: Point[], invCenters: Point[], sides: number, i_ra
 	}
 
 	return [newCenters, newInvCenters];
+}
+
+function genCentersVS(centers: Point[], sides: number, i_rad: [number, number]) {
+	const newCenters: Point[] = [];
+	for (const center of centers) {
+		let dir_0 = rotate([0, -i_rad[0]], -Math.PI / sides);
+		let dir_1 = rotate([0, -i_rad[1]], 0);
+		for (let j = 0; j < sides; j++) {
+			newCenters.push(add(center, dir_0));
+			newCenters.push(add(center, dir_1));
+			dir_1 = rotate(dir_1, (2 * Math.PI) / sides);
+			dir_0 = rotate(dir_0, (2 * Math.PI) / sides);
+		}
+	}
+	return newCenters;
 }
 
 function polyVGen(center: Point, sides: number, o_rad: number, shift = 0) {
@@ -168,6 +183,51 @@ const HexNut: FillFractalInfo = {
 	},
 };
 
+const HexPool: FillFractalInfo = {
+	name: "Hex Pool",
+	maxDepth: 5,
+	image: fibonacciWordFractal,
+	rules: () => {
+		const sides = 6;
+		const ratio = 1 / 3;
+		const o_rad = 900;
+		let i_rad = o_rad * (1 - ratio) * Math.cos(Math.PI / 6);
+		return {
+			origin: [0, 0],
+			rules: (centers, invCenters) => {
+				const allcenters = genCentersVC(centers, invCenters, sides, i_rad, Math.PI / 6);
+				i_rad *= ratio;
+				return allcenters;
+			},
+			polyVGen: (d, flip) => (c: Point) => polyVGen(c, sides, o_rad * Math.pow(ratio, d), flip ? Math.PI : 0),
+		};
+	},
+};
+
+const SierpinskiCarpet: FillFractalInfo = {
+	name: "Sierpinski Carpet",
+	maxDepth: 5,
+	image: fibonacciWordFractal,
+	rules: () => {
+		const o_rad = 900 * Math.sqrt(2);
+		const ratio = 1 / 3;
+		const sides = 4;
+		let i_rad: [number, number] = [o_rad * (1 - ratio), (o_rad * (1 - ratio)) / Math.sqrt(2)];
+		return {
+			origin: [0, 0],
+			polyVGen: (d) => {
+				return (c: Point) => polyVGen(c, 4, o_rad * Math.pow(ratio, d), Math.PI / 4);
+			},
+			rules: (centers, invCenters) => {
+				const newCenters = genCentersVS(centers, sides, i_rad);
+				i_rad[0] *= ratio;
+				i_rad[1] *= ratio;
+				return [newCenters, invCenters];
+			},
+		};
+	},
+};
+
 const FillFractalRuleSet: Record<string, FillFractalInfo> = {
 	"sierpinski-triangle": SierpinskiTriangle,
 	"sierpinski-pentagon": SierpinskiPentagon,
@@ -175,6 +235,8 @@ const FillFractalRuleSet: Record<string, FillFractalInfo> = {
 	"penta-flake": PentaFlake,
 	"hexa-flake": HexaFlake,
 	"hex-nut": HexNut,
+	"hex-pool": HexPool,
+	"sierpinski-carpet": SierpinskiCarpet,
 };
 
 export default FillFractalRuleSet;
