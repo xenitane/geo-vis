d889af426606a03627796ff586b9173481ca2270
xenitane <xenitane@users.noreply.github.com>
2024-02-11 11:14:02 +0000

    deploy: b5d9f410d5364e2180a3175d032afe846fa88c4b

--- assets/Home-ncwSq41I.js
+++
@@ -1,8 +0,0 @@
-import{j as e,S as a,r,_ as o,c as l}from"./index-ZZnzyjVk.js";import{L as c}from"./Linear-B-RYrmYL.js";import{F as i}from"./Fill-5kQz1DTr.js";import{B as n}from"./Branching-l4YYxn5q.js";import{A as m}from"./Attractor-WVTKvxIg.js";import"./complex-w3zDMYH0.js";import"./fracUtils-EuSe4TLi.js";const p={Linear:c,Fill:i,Branching:n,Attractor:m},x=()=>e.jsxs("div",{className:" space-y-4",children:[e.jsx(a,{className:"h-9 w-64 rounded-2xl"}),e.jsx(a,{className:"h-0.5 w-full"})]}),u=r.lazy(()=>o(()=>import("./GeoSection-FjYxosu_.js"),__vite__mapDeps([0,1,2]))),L=()=>e.jsx("article",{className:l("flex flex-col gap-8"),children:Object.entries(p).map(([t,s])=>e.jsx(r.Suspense,{fallback:e.jsx(x,{}),children:e.jsx(u,{sectionId:t,objectList:s})},t))});export{L as default};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["assets/GeoSection-FjYxosu_.js","assets/index-ZZnzyjVk.js","assets/index-5LVVaGSU.css"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-//# sourceMappingURL=Home-ncwSq41I.js.map
