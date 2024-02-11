4b69181a5fc2325f158b66f3d50345d7786bc211
Tushar Jain | xenitane <tushar01.tjdsk@gmail.com>
2024-02-11 16:39:27 +0530

    added sierpinski carpet outline

--- src/assets/Thumbnails/branching/index.ts
+++ src/assets/Thumbnails/branching/index.ts
@@ -2,5 +2,6 @@ import sierpinskiCarpet from "@/assets/Thumbnails/branching/sierpinski-carpet.sv
 import sierpinskiTriangleSkeleton from "@/assets/Thumbnails/branching/sierpinski-triangle-skeleton.svg";
 import tSquare from "@/assets/Thumbnails/branching/t-square.svg";
 import vicsekFractal from "@/assets/Thumbnails/branching/vicsek-fractal.svg";
+import peanoSierpinskiCarpet from "@/assets/Thumbnails/branching/peano-sierpinski-carpet.svg";
 
-export { sierpinskiCarpet, sierpinskiTriangleSkeleton, tSquare, vicsekFractal };
+export { sierpinskiCarpet, sierpinskiTriangleSkeleton, tSquare, vicsekFractal, peanoSierpinskiCarpet };
