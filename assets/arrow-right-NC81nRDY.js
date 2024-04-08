02616033a285889a49b50873d08fbc124c10b65d
xenitane <xenitane@users.noreply.github.com>
2024-04-08 12:22:00 +0000

    deploy: 11e63b53786fe213f5107902077a19fb9854dc68

--- assets/arrow-right-NC81nRDY.js
+++ assets/arrow-right-NC81nRDY.js
@@ -1,4 +1,4 @@
-import{n as t}from"./GlobalLayout-BeWW7bl7.js";/**
+import{n as t}from"./GlobalLayout-DmVCJEGm.js";/**
  * @license lucide-react v0.363.0 - ISC
  *
  * This source code is licensed under the ISC license.
