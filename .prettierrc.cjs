545a6c073374470a59a68df7d975af63c9784d6f
Tushar Jain | xenitane <tusha01.tjdsk@gmail.com>
2025-07-10 19:27:52 +0530

    base setup for astro

--- .prettierrc.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-
-module.exports = {
-    useTabs: false,
-    tabWidth: 4,
-    trailingComma: "es5",
-    printWidth: 128,
-    plugins: ["prettier-plugin-tailwindcss"],
-};
