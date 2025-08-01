ab7f5fea6f798f9ab10e3dbe9852fe1e57dcdfe1
xenitane <tushar01.tjdsk@gmail.com>
2025-08-01 18:31:42 +0530

    save as png

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,3 @@
 import { DOMAIN_NAME } from "astro:env/client";
 // for layout
 export const SITE_TITLE = `Geometric Visualizer | Xenitane (xenitane) | ${DOMAIN_NAME}`;
-export const SAVE_AS_PNG = false;
